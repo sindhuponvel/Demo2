9763f87d50fb7ebd215db9c0237d17d7bfc9a5df
Chethan1306 <166906416+Chethan1306@users.noreply.github.com>
2024-06-01 12:16:54 +0530

    Newly added parameters

--- Demo2/src/test/java/demo1/Demo1Test.java
+++ Demo2/src/test/java/demo1/Demo1Test.java
@@ -7,6 +7,14 @@ public class Demo1Test {
 	@Test(groups="Smoke Test")
 	public void createObjectTest()
 	{
+		String URL = System.getProperty("Url");
+		String BROWSER = System.getProperty("Browser");
+		String USERNAME = System.getProperty("Username");
+		String PASSWORD = System.getProperty("Password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 	System.out.println("Execute create object");	
 	}
 	@Test
